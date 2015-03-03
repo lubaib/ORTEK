907971cf4354494b1d9f11733e408076d1a40b35
Ceejey <lubaib@carmatec.com>
2015-03-03 19:51:12 +0530

    ortek app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrtekApp::Application.config.secret_key_base = 'da97b0ead7050246552c25ba1a936037b20b0a6e1fb67d525c9fe58ae6f534d7fdcbfe935e5b3fcc4b89f8f92eabba10f6a2733efee77b578ffc51289ae49fa0'
